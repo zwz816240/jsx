4a9711ead6d24ac785f50b84912d1b19eb1804d4
zwz816240 <55817717+zwz816240@users.noreply.github.com>
2019-12-28 16:58:37 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import "./App.css"
+export default class App extends Component {
+  render() {
+    // let titles = ['Pull request','Issues','Maketplace','Explore'];
+    // let list_items = [];
+    // titles.forEach((v,i)=>list_items.push(<li key={i}>{v}</li>))
+    let is_boy = true
+    let hefen = {
+      name : "小李",
+      price : 100
+    };
+    const fn = function(msg){
+      return "..........."+msg+"..........."
+    }
+    return (
+      // <div>
+      //   <ul>
+      //     <li>Pull request</li>
+      //     <li>Issues</li>
+      //     <li>Maketplace</li>
+      //     <li>Explore</li>
+      //   </ul>
+      // </div>
+      // <div>
+      //   <ul>
+      //     {
+      //       // titles.forEach((v,i)=><li key={i}>{v}</li>)
+      //       // titles.map((v,i)=><li key={i}>{v}</li>)
+      //       list_items
+      //     }
+      //   </ul>
+      // </div>
+      <div>
+        {/* 普通字符串 */}
+        <h1>{"我是帅哥"}</h1>
+        {/* 算数表达式 */}
+        <h1>1+1={1+1}</h1>
+        {/* 三元表达式 */}
+        {
+          is_boy?"男":"女"
+        }
+        <br/>
+        {/* 渲染对象 */}
+        {
+          hefen.price
+        }
+        <br/>
+        {/* 执行函数 */}
+        {
+          fn("天气好热啊")
+        }
+      </div>
+    )
+  }
 }
-
-export default App;
